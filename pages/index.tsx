2a31536f30c8e95ec2b58b9f50e5df302d0ac4c8
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-04-27 17:45:18 -0300

    inserção dos arquivos iniciais do projeto e para  o MUI

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Foooter';
+
+export default function Main() {
+  return (
+    <div>
+      <Header />
+      <main>Hellow</main>
+      <Footer />
+    </div>
+  );
+}
