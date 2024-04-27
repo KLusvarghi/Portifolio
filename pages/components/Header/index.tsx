5064208abbd2a83321a39e22281a3da87cb6b313
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-04-27 18:14:31 -0300

    inserção dos componentes do Header

--- pages/components/Header/index.tsx
+++ pages/components/Header/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <Image
+      // o "src" se deve passar a partir da página public, não precisando voltar página por página
+        src="/logo.svg"
+        alt="Logo do site"
+        width={100}
+        height={20}
+        onError={(e) => console.error("error: ", e.target)} //caso de erro ele exibe no console
+      />
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Sobre Mim</li>
+          <li>Projetos</li>
+          <li>Contato</li>
+        </ul>
+      </div>
+    </div>
   )
 }
