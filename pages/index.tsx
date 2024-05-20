919131c491510ebbfe37f5a93355b55fe69d5b9c
klusvarghi <71743008+KLusvarghi@users.noreply.github.com>
2024-05-20 20:34:08 -0300

    inicio da intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Foooter';
 import Head from './infra/components/Head';
+import styles from './Main.module.scss';
 
 export default function Home() {
   return (
     // <div style={{background: 'black'} }>
-    <div>
+    <div className={styles.main}>
       <Head title="Home - Portifólio Kauã Ortolani Lusvarghi" />
       <Header />
       <main>Hellow</main>
